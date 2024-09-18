c80d130a874567038b918230b4135ea78df8c399
kiing12 <reid.paulosilva@gmail.com>
2024-09-18 13:55:45 +0000

    criaçao de prompt

---
+++ atividade.js
@@ -0,0 +1,15 @@
+
+/*const nome = "Reid. Paulo"
+let idade = "22" 
+console.log ("Ola, me chamo Reideclildon Paulo, moro em Recife e tenho 22 anos de idade"); /*/
+
+
+/*const nome = "iputinga"
+const cidade = "Recife"
+console.log ("Iputinga é um bairro de Recife, cidade em Pernambuco"); /*/
+
+const dividir = (10/5)
+console.log (dividir)
+
+const soma = (10+5)
+console.log (soma)
---
+++ aula.js
@@ -0,0 +1,20 @@
+/*var prompt = require ('prompt-sync')();
+
+let nome = prompt ('qual o seu nome?')
+
+console.log(`Ola, ${nome} !! Boas vindas a faculdade senac`);/*/
+
+
+var prompt = require ('prompt-sync')();
+
+let nome = prompt ('Ola, qual o seu nome?')
+
+console.log(`Ola, ${nome}!! Bem vindo ao atendiemtno de tattoo`);
+
+let serviço = prompt ('Qual serviço voce deseja?')
+
+console.log(`Maravilha ${nome}!! Me informa qual projeto voce gostaria de realizar`);
+
+let valor = prompt ('seria um fechamento de antebraço com uma imagem de um leao, quanto ficaria?')
+
+console.log(`O valor dos eu projeto fica R$1.000`);
